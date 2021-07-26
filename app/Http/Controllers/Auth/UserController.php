5ea36d72fc775053780f6ffe2e3666010758a2ab
Aladdin Mohammed <alaa4455@gmail.com>
2021-07-26 10:30:23 +0300

    add role to user model and edit test

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -16,5 +18,19 @@ class UserController extends Controller
     public function current(Request $request)
     {
         return response()->json($request->user());
+    }
+    public function update(Request $request)
+    {
+
+        $user = Auth::user();
+        $user->update($request->validate([
+            'name' => 'required',
+            'email' => 'required|Email',
+            'phone_number' => 'nullable|digits:9',
+            'country_id' => 'nullable|integer',
+            'city_id' => 'nullable|integer',
+            'district' => 'nullable|string'
+        ]));
+
     }
 }
