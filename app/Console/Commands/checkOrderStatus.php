a6babc7e0df01d79015943785ca7382a7a856e02
Aladdin Mohammed <alaa4455@gmail.com>
2021-08-09 21:47:55 +0300

    Feature: define command and schedule worker for checking order status

---
+++ app/Console/Commands/checkOrderStatus.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Order;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class checkOrderStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'check:order';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'check order status and change it if needed';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $offering_done = Order::all()->where('offering_end_at' , '<=' , Carbon::now())->where('status' , '==' , '3')->pluck('id');
+        Order::whereIn('id',$offering_done)->update(['status' => '4']);
+        $share_done = Order::all()->where('share_end_at' , '<=' , Carbon::now())->where('status' , '==' , '2')->pluck('id');
+         Order::whereIn('id' ,$share_done)->update(['status' => '3']);
+        $vote_done = Order::all()->where('vote_end_at' , '<=' , Carbon::now())->where('status' , '==' , '4')->pluck('id');
+         Order::whereIn('id',$vote_done)->update(['status' => '5']);
+
+         $this->info('order has been checked successfully');
+
+    }
+}
